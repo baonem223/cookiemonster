0c1126b74f5c4ce6a86535c43223f8efa88ad76c
baonem223 <kevinbao59@gmail.com>
2026-09-03 13:40:15 -0500

    Add navigation items to nav.js

---
+++ nav.js
@@ -0,0 +1,6 @@
+const navItems = [
+  { label: "About Me", href: "index.html#about", page: "about" },
+  { label: "Research", href: "research.html", page: "research" },
+  { label: "Teaching", href: "teaching.html", page: "teaching" },
+  { label: "Personal", href: "personal.html", page: "personal" }
+];
